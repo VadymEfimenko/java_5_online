795af3559519c0e02cf6d7b055edd2ea481dc9a7
vadim <vadimpproduction@gmail.com>
2023-03-12 10:24:15 +0200

    1

--- hw_6_stream/src/main/java/ua/com/alevel/Main.java
+++ hw_6_stream/src/main/java/ua/com/alevel/Main.java
@@ -1,7 +1,10 @@
 package ua.com.alevel;
 
+import org.w3c.dom.Text;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TextStatistic textStatistic = new TextStatistic();
+        System.out.println(textStatistic.rating(textStatistic.toList("hello hello world, how are you bitch?")));
     }
 }
