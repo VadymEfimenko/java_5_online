3a973914a0af93ba7c6a3f5df62066a56b6fd6ad
vadim <vadimpproduction@gmail.com>
2023-04-03 15:52:08 +0300

    hw_6

--- hw_6_stream/src/main/java/ua/com/alevel/Main.java
+++ hw_6_stream/src/main/java/ua/com/alevel/Main.java
@@ -1,10 +1,6 @@
 package ua.com.alevel;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.function.Function;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
