62c50f9bc14b0659eb9487f4f3878907f1c0a5ab
vadim <vadimpproduction@gmail.com>
2023-04-04 15:47:42 +0300

    hw_8

---
+++ hw_8_crud_file/src/main/java/ua/com/alevel/Hw8CrudFileApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw8CrudFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw8CrudFileApplication.class, args);
+    }
+
+}
