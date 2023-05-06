2118a47b6bc09bad860df10738e77a3b4af024e0
vadim <vadimpproduction@gmail.com>
2023-05-06 22:17:37 +0300

    hw_10_start

---
+++ hw_10_crud_hibernate/src/main/java/ua/com/alevel/Hw10CrudHibernateApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw10CrudHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw10CrudHibernateApplication.class, args);
+	}
+
+}
